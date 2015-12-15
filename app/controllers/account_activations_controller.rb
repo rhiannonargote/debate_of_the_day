0592619b9188c056efbfc7f130966dd2f38082b2
Rhiannon Argote <rhiannon.argote@gmail.com>
2015-12-15 11:22:07 +1100

    wtf

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -20,7 +20,6 @@ class AccountActivationsController < ApplicationController
 
   def edit
     user = User.find_by(email: params[:email])
-    raise "fuuu"
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.update_attribute(:activated,    true)
       user.update_attribute(:activated_at, Time.zone.now)
