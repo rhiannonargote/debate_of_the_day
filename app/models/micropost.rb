f199498cdee3f4dc80ac1127a620230a4aee9e9e
Rhiannon Argote <rhiannon.argote@gmail.com>
2015-12-17 16:54:52 +1100

    fuck this

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -6,6 +6,7 @@
 #  content     :text
 #  user_id     :integer
 #  question_id :integer
+#  for         :boolean
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #  picture     :string
